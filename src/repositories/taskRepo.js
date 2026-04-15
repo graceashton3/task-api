5ca1084ed6e8953997bc342f50bc6c46374ba119
graceashton <mashton1@charlotte.edu>
2026-04-14 22:58:54 -0400

    Add rest of GET requirements

--- src/repositories/taskRepo.js
+++ src/repositories/taskRepo.js
@@ -1,7 +1,13 @@
 import prisma from '../config/db.js';
 
-export async function findAll() {
-  return prisma.task.findMany();
+export async function findAll(filters = {}) {
+  const where = {};
+
+  if (filters.completed !== undefined) {
+    where.completed = filters.completed;
+  }
+
+  return prisma.task.findMany({ where });
 }
 
 // Create a new task
--- src/services/taskService.js
+++ src/services/taskService.js
@@ -1,7 +1,15 @@
 import * as taskRepository from '../repositories/taskRepo.js';
 
-export async function getAllTasks() {
-  return taskRepository.findAll();
+export async function getAllTasks(filters = {}) {
+  let completed;
+
+  if (filters.completed === 'true') {
+    completed = true;
+  } else if (filters.completed === 'false') {
+    completed = false;
+  }
+
+  return taskRepository.findAll({ completed });
 }
 
 export async function createTask(newTask) {
